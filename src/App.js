04a4e98de8d920793ac82aa0c244e9db72d04457
kesvesna <kesvesna@rambler.ru>
2021-10-13 20:14:14 +0300

    Lesson3.3-material-ui, without autofocus

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Stack from '@mui/material/Stack';
 import SearchIcon from '@mui/icons-material/Search';
 import SendIcon from '@mui/icons-material/Send';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import {createTheme, ThemeProvider} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 
 export function App(props) {
@@ -35,7 +35,7 @@ export function App(props) {
     const inputMessageRef = useRef(null);
 
     const handleOnClickSendButton = () => {
-        if(message){
+        if (message) {
             let lastMessageId = messageList.length;
             setMessageList(messageList => ([...messageList, {
                 messageId: lastMessageId++,
@@ -50,7 +50,7 @@ export function App(props) {
 
     useEffect(() => {
         let timer = null;
-        const lastMessage = messageList[messageList.length-1];
+        const lastMessage = messageList[messageList.length - 1];
         if (messageList.length > 1 && lastMessage.author !== "Bot") {
             timer = setTimeout(() => {
                 let lastMessageId = messageList.length;
@@ -88,44 +88,43 @@ export function App(props) {
 
     return (
 
-            <ThemeProvider theme={theme}>
-                <div className="App">
-            <div className="container-fluid">
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <div className="container-fluid">
                     <Container className="left-side-block bg-light">
                         <Stack direction="row" className="search-row">
-                            <TextField size="small" fullWidth label="Найти" variant="outlined" />
+                            <TextField size="small" fullWidth label="Найти" variant="outlined"/>
                             <Button className="search-button" variant="outlined"><SearchIcon/></Button>
                         </Stack>
                         <List className="left-side-block-chat-info">
                             {chatList.map((item) => (
                                 <ListItem disablePadding key={item.id}>
                                     <ListItemButton>
-                                        <ListItemText primary={item.chatName} />
+                                        <ListItemText primary={item.chatName}/>
                                     </ListItemButton>
                                 </ListItem>
                             ))}
                         </List>
                     </Container>
-                <div className="right-side-block col-8 bg-light">
-                    <Typography className="chat-name">Чат 1</Typography>
-                    <div className="right-side-block-chat-message">
-                        {messageList.map((item) => (
-                            <div className={item.styleInfo} key={item.messageId}>
-                                <p className="align-self-center">{item.message}</p>
-                            </div>
-                        ))}
-                    </div>
+                    <div className="right-side-block col-8 bg-light">
+                        <Typography className="chat-name">Чат 1</Typography>
+                        <div className="right-side-block-chat-message">
+                            {messageList.map((item) => (
+                                <div className={item.styleInfo} key={item.messageId}>
+                                    <p className="align-self-center">{item.message}</p>
+                                </div>
+                            ))}
+                        </div>
                         <Stack direction="row">
                             <TextField fullWidth label="Введите текст сообщения" variant="outlined"
-                                       ref={inputMessageRef}  value={message}
+                                       ref={inputMessageRef} value={message}
                                        onChange={(e) => setMessage(e.target.value)}/>
                             <Button variant="outlined" onClick={handleOnClickSendButton}><SendIcon/></Button>
                         </Stack>
+                    </div>
                 </div>
             </div>
-                </div>
-            </ThemeProvider>
-
+        </ThemeProvider>
     );
 }
 
