1e4e4c359ae046be988f431ed6d4b27495e2a2d6
kesvesna <kesvesna@rambler.ru>
2021-10-17 15:17:02 +0300

    Lesson4.3-children-route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import * as React from 'react';
 import {useState, useEffect, useRef} from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import SearchIcon from '@mui/icons-material/Search';
 import SendIcon from '@mui/icons-material/Send';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@material-ui/core/styles';
+import {styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import InboxIcon from '@mui/icons-material/Inbox';
-import { Chip } from '@mui/material';
+import {Chip} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { ChatList } from './components/ChatList/ChatList.js'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Link from "@mui/material/Link";
+import {ChatList} from './components/ChatList/ChatList'
+import {MessageList} from "./components/MessageList/MessageList";
 
 
 const theme = createTheme({
@@ -48,112 +44,195 @@ export function App() {
 
     const inputRef = useRef(null);
 
-    const [messageList, setMessageList] = useState([]);
+    const inputAddChat = useRef(null);
 
     const [message, setMessage] = useState('');
 
-    const [chatList, setChatList] = useState([
-        {id: 1, chatName: 'Чат 1'}, {id: 2, chatName: 'Чат 2'}, {id: 3, chatName: 'Чат 3'}, {id: 4, chatName: 'Чат 4'},
-        {id: 5, chatName: 'Чат 5'}, {id: 6, chatName: 'Чат 6'}, {id: 7, chatName: 'Чат 7'}, {id: 8, chatName: 'Чат 8'},
-        {id: 9, chatName: 'Чат 9'}, {id: 10, chatName: 'Чат 10'}, {id: 11, chatName: 'Чат 11'},
-        {id: 12, chatName: 'Чат 12'}, {id: 13, chatName: 'Чат 13'},
-        {id: 14, chatName: 'Чат 14'}, {id: 15, chatName: 'Чат 15'},
-        {id: 16, chatName: 'Чат 16'}
-    ]);
+    const [addChat, setChatAdd] = useState('');
+
+    const [chats, setChats] = useState({
+        id1: {
+            name: 'Chat 1', messages: [
+                {id: 1, author: 'User', message: 'Message 1', date: new Date()},
+                {id: 2, author: 'Bot', message: 'Message 1', date: new Date()},
+                {id: 3, author: 'User', message: 'Message 2', date: new Date()},
+                {id: 4, author: 'Bot', message: 'Message 2', date: new Date()}
+            ]
+        },
+        id2: {
+            name: 'Chat 2', messages: [
+                {id: 1, author: 'User', message: 'Message 3', date: new Date()},
+                {id: 2, author: 'Bot', message: 'Message 3', date: new Date()},
+                {id: 3, author: 'User', message: 'Message 4', date: new Date()},
+                {id: 4, author: 'Bot', message: 'Message 4', date: new Date()}
+            ]
+        },
+        id3: {
+            name: 'Chat 3', messages: [
+                {id: 1, author: 'User', message: 'Message 5', date: new Date()},
+                {id: 2, author: 'Bot', message: 'Message 5', date: new Date()},
+                {id: 3, author: 'User', message: 'Message 6', date: new Date()},
+                {id: 4, author: 'Bot', message: 'Message 6', date: new Date()}
+            ]
+        },
+    });
+
+    const [currentChat, setCurrentChat] = useState('Choose chat');
+
+    const updateCurrentChat = (chatId) => {
+        setCurrentChat(chatId);
+    }
+
+    const deleteChat = (chatId) => {
+        let newChats = { ...chats };
+        delete newChats[chatId];
+        setChats(newChats);
+        setCurrentMessages([]);
+        setCurrentChat('Choose chat');
+    }
+
+    const [currentMessages, setCurrentMessages] = useState([]);
+
+    useEffect(() => {
+        let temp = [];
+        try {
+            temp = chats[currentChat].messages;
+        } catch (e) {
+        }
+        return setCurrentMessages(temp);
+    }, [currentChat]);
 
     const handleOnClickSendButton = () => {
         if (message) {
-            let lastMessageId = messageList.length;
-            setMessageList(messageList => ([...messageList, {
-                chatId: 1,
-                messageId: lastMessageId++,
-                author: "User1",
-                message: message
-            }]));
+            let lastMessageId = chats[currentChat].messages.length;
+            setChats({
+                ...chats,
+                [currentChat]: {
+                    ...chats[currentChat],
+                    messages: [
+                        ...chats[currentChat].messages, {
+                            id: lastMessageId++,
+                            message: message,
+                            author: 'User',
+                            date: new Date()
+                        }
+                    ]
+                }
+            });
+            setCurrentMessages([...currentMessages, {
+                id: lastMessageId++,
+                message: message,
+                author: 'User',
+                date: new Date()
+            }]);
             setMessage('');
         }
         inputRef.current?.focus();
     }
 
+    const handleClickOnChatAddButton = () => {
+        if (addChat) {
+            let lastChatId = Object.keys(chats)[Object.keys(chats).length - 1];
+            console.log(lastChatId);
+            if(lastChatId !== undefined){
+                lastChatId = 'id' + (parseInt(lastChatId.substr(2)) + 1);
+            } else {
+                lastChatId = 'id1';
+            }
+            setChats({
+                ...chats, [lastChatId]:
+                    {
+                        name: addChat, messages: []
+                    }
+            });
+            setChatAdd('');
+        }
+    }
+
     useEffect(() => {
         let timer = null;
-        const lastMessage = messageList[messageList.length - 1];
-        if (messageList.length >= 1 && lastMessage.author !== "Bot") {
+        const lastMessage = currentMessages[currentMessages.length - 1];
+        if (currentMessages.length >= 1 && lastMessage.author !== "Bot") {
             timer = setTimeout(() => {
-                let lastMessageId = messageList.length;
-                setMessageList(messageList => ([...messageList, {
-                    chatId: 1,
-                    messageId: lastMessageId++,
+                let lastMessageId = currentMessages.length;
+                setChats({
+                    ...chats,
+                    [currentChat]: {
+                        ...chats[currentChat],
+                        messages: [
+                            ...chats[currentChat].messages, {
+                                id: lastMessageId++,
+                                author: "Bot",
+                                message: "Bot message",
+                                date: new Date()
+                            }
+                        ]
+                    }
+                });
+                setCurrentMessages(currentMessages => ([...currentMessages, {
+                    id: lastMessageId++,
                     author: "Bot",
-                    message: "Bot message"
+                    message: "Bot message",
+                    date: new Date()
                 }]));
             }, 1500);
         }
         inputRef.current?.focus();
         return () => clearTimeout(timer)
-    }, [messageList]);
+    }, [currentMessages]);
 
 
     return (
         <ThemeProvider theme={theme}>
             <div className="App">
                 <Box sx={{flexGrow: 1}}>
-                    <AppBar position="static">
-                        <Toolbar>
-                            <List style={{ display: 'flex'}}>
-                                <ListItem>
-                                    <Link href="/" style={{ color: 'white' }}>Home</Link>
-                                </ListItem>
-                                <ListItem>
-                                    <Link href="/chat" style={{ color: 'white' }}>Chat</Link>
-                                </ListItem>
-                                <ListItem>
-                                    <Link href="/profile" style={{ color: 'white' }}>Profile</Link>
-                                </ListItem>
-                            </List>
-                        </Toolbar>
-                    </AppBar>
                     <Grid container spacing={1}>
                         <Grid item xs={3}>
                             <Item style={{height: '10vh'}}>
-                                <Stack direction="row" style={{ alignItems: 'center'}}>
+                                <Stack direction="row" style={{alignItems: 'center'}}>
                                     <TextField fullWidth id="standard-basic" label="Поиск" variant="standard"/>
                                     <SearchIcon/>
                                 </Stack>
                             </Item>
                             <Item style={{height: '10vh'}}>
-                                <Stack direction="row" style={{ alignItems: 'center'}}>
-                                    <TextField fullWidth id="standard-basic" label="Добавить чат" variant="standard"/>
-                                    <AddIcon/>
+                                <Stack direction="row" style={{alignItems: 'center'}}>
+                                    <TextField inputRef={inputAddChat} value={addChat}
+                                               onChange={(e) => setChatAdd(e.target.value)}
+                                               fullWidth id="standard-basic" label="Добавить чат" variant="standard"/>
+                                    <AddIcon onClick={handleClickOnChatAddButton}/>
                                 </Stack>
                             </Item>
                             <Item style={{height: '58vh', overflowX: 'auto'}}>
-                                <ChatList />
+                                <ChatList chats={chats} updateCurrentChat={updateCurrentChat} currentChat={currentChat}
+                                          deleteChat={deleteChat}/>
                             </Item>
                         </Grid>
                         <Grid item xs={9}>
-                            <Item style={{ alignItems: 'center', height: '10vh'}}>
-                                <Chip icon={<InboxIcon/>} label="Чат 1" variant="outlined"/>
+                            <Item style={{alignItems: 'center', height: '10vh'}}>
+                                <Chip icon={<InboxIcon/>} label={currentChat} variant="outlined"/>
                             </Item>
                             <Item style={{height: '58vh', overflowX: 'auto'}}>
-                                {messageList.map((item) => (
+                                {currentMessages.map((item) => (
                                     (item.author !== "Bot" &&
-                                        <Stack key={item.messageId} display="row"  style={{ alignItems: 'flex-end'}} marginBottom="10px">
-                                            <Item key={item.messageId} style={{width: '50%'}}>
+                                        <Stack key={item.id} display="row" style={{alignItems: 'flex-end'}}
+                                               marginBottom="10px">
+                                            <Item key={item.id} style={{width: '50%'}}>
                                                 {item.message}
                                             </Item>
                                         </Stack>)
                                     ||
                                     (item.author === "Bot" &&
-                                        <Stack  key={item.messageId} display="row"  style={{ alignItems: 'flex-start'}} marginBottom="10px">
-                                            <Item  key={item.messageId} style={{width: '50%'}}>
+                                        <Stack key={item.id} display="row" style={{alignItems: 'flex-start'}}
+                                               marginBottom="10px">
+                                            <Item key={item.id} style={{width: '50%'}}>
                                                 {item.author + ": "}{item.message}
                                             </Item>
                                         </Stack>)
-                                ))}
+                                ))
+                                }
                             </Item>
                             <Item style={{height: '10vh'}}>
-                                <Stack direction="row" style={{ alignItems: 'center'}}>
+                                <Stack direction="row" style={{alignItems: 'center'}}>
                                     <TextField inputRef={inputRef} value={message}
                                                onChange={(e) => setMessage(e.target.value)}
                                                fullWidth id="standard-basic" label="Введите сообщение"
--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,28 +1,26 @@
-import { useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import * as React from 'react';
 import './ChatList.css';
+import { Link } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
 
-export function ChatList() {
-
-    const [chatList, setChatList] = useState([
-        {id: 1, chatName: 'Чат 1'}, {id: 2, chatName: 'Чат 2'}, {id: 3, chatName: 'Чат 3'}, {id: 4, chatName: 'Чат 4'},
-        {id: 5, chatName: 'Чат 5'}, {id: 6, chatName: 'Чат 6'}, {id: 7, chatName: 'Чат 7'}, {id: 8, chatName: 'Чат 8'},
-        {id: 9, chatName: 'Чат 9'}, {id: 10, chatName: 'Чат 10'}, {id: 11, chatName: 'Чат 11'},
-        {id: 12, chatName: 'Чат 12'}, {id: 13, chatName: 'Чат 13'},
-        {id: 14, chatName: 'Чат 14'}
-    ]);
+export function ChatList({chats, updateCurrentChat, deleteChat}) {
 
     return (
         <List>
-            {chatList.map((item) => (
-                <ListItem disablePadding  key={item.id} >
-                    <ListItemButton>
-                        <ListItemText primary={item.chatName}/>
+            {Object.keys(chats).map((item, index) => (
+                <ListItem disablePadding  key={item}>
+                    <ListItemButton  onClick={() => { updateCurrentChat(item)}}>
+                        <Link style={{ textDecoration: 'none' }}
+                              to={"/chats/" + item}>
+                            <ListItemText primary={chats[item].name}/>
+                        </Link>
                     </ListItemButton>
+                    <CloseIcon onClick={() => { deleteChat(item)}}/>
                 </ListItem>
             ))}
         </List>
